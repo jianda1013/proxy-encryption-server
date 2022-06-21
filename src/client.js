13b86efe359e9c76b33672f73c5c8e0bfe059f41
jianda <jianda851013@gmail.com>
2022-06-21 22:29:40 +0800

    [change] add diffie helleman key excahgne

--- src/client.js
+++ src/client.js
@@ -1,12 +1,32 @@
 const httpProxy = require('http-proxy');
 const http = require('http');
 const express = require('express');
-let CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js");
+const { createDiffieHellman } = require('node:crypto');
+const fetch = require('node-fetch');
+const config = require('./client_config.json');
 
 const proxy = httpProxy.createProxyServer({})
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+let target = 'http://127.0.0.1:8080'
+
+const client_base = createDiffieHellman(config.prime, 2);
+const public_key = client_base.generateKeys().toString("hex");
+let share_key = null;
+
+const keyGen = async () => {
+    let response = await fetch(`${target}/key_exchange`, {
+        method: 'POST',
+        body: JSON.stringify({ "key": public_key }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+    share_key = await response.json()
+    share_key = client_base.computeSecret(Buffer.from(share_key.key, "hex"));
+    return;
+}
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
     if (req.method === "POST" && req.body) {
@@ -18,22 +38,39 @@ proxy.on("proxyReq", (proxyReq, req, res) => {
 })
 
 proxy.on("proxyRes", (proxyRes, req, res) => {
-    let _write = res.write, output, body = "";
+    let _write = res.write, body = "";
     proxyRes.on('data', data => {
-        data = JSON.parse(data.toString('utf-8'))?.data;
-        data = CryptoJS.AES.decrypt(data, "test").toString(CryptoJS.enc.Utf8);
-        body += data;
+        data = data.toString('utf-8');
+        try {
+            data = JSON.parse(data)?.data;
+            data = CryptoJS.AES.decrypt(data, share_key.toString("hex")).toString(CryptoJS.enc.Utf8);
+            body = JSON.parse(data);
+        } catch (err) {
+            res.end(data);
+        }
     });
     res.write = () => {
-        eval("output=" + body)
-        res.setHeader('content-length', output.length);
-        _write.call(res, output);
+        if (body.length) {
+            res.setHeader('content-length', body.length);
+            _write.call(res, body);
+        }
     }
 })
 
-app.use((req, res) => {
-    req.body = { data: CryptoJS.AES.encrypt(JSON.stringify(req.body), "test").toString() };
-    proxy.web(req, res, { target: 'http://127.0.0.1:8080' })
+proxy.on("error", (err, req, res) => {
+    res.end(err.message);
+})
+
+app.post('/key_exchange', (req, res) => {
+    keysMap[req.ip] = server_base.computeSecret(Buffer.from(req.body.key, "hex"));
+    console.log(keysMap[req.ip].toString("hex"));
+    res.json({ key: master_public_key });
+})
+
+app.use(async (req, res) => {
+    if (!share_key) await keyGen();
+    req.body = { data: CryptoJS.AES.encrypt(JSON.stringify(req.body), share_key.toString("hex")).toString() };
+    proxy.web(req, res, { target })
 });
 
 http.createServer(app).listen(1337, '0.0.0.0', () => {
--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 const httpProxy = require('http-proxy');
 const http = require('http');
 const express = require('express');
-let CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js");
+const { createDiffieHellman } = require('node:crypto');
+const fetch = require('node-fetch');
+const config = require('./server_config.json');
+
+const keysMap = {};
 
 const proxy = httpProxy.createProxyServer({})
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const server_base = createDiffieHellman(config.prime, 2);
+const master_public_key = server_base.generateKeys().toString("hex");
+
+// const keyGen = async (ip) => {
+//     let response = await fetch(`${ip}/key_exchange`, {
+//         method: 'POST',
+//         body: JSON.stringify({ "key": master_public_key }),
+//         headers: { 'Content-Type': 'application/json' }
+//     });
+//     let share_key = await response.json()
+//     keysMap[ip] = client_base.computeSecret(Buffer.from(share_key.key, "hex"));
+//     return;
+// }
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
     if (req.method === "POST" && req.body) {
@@ -18,21 +37,34 @@ proxy.on("proxyReq", (proxyReq, req, res) => {
 })
 
 proxy.on("proxyRes", (proxyRes, req, res) => {
-    let _write = res.write, output, body = "";
+    let _write = res.write, body = "";
     proxyRes.on('data', data => {
         data = data.toString('utf-8');
-        data = { data: CryptoJS.AES.encrypt(JSON.stringify(data), "test").toString() };
-        body += JSON.stringify(data);
+        data = { data: CryptoJS.AES.encrypt(JSON.stringify(data), keysMap[req.ip].toString("hex")).toString() };
+        body = JSON.stringify(data);
     });
     res.write = () => {
-        eval("output=" + body)
-        res.setHeader('content-length', JSON.stringify(output).length);
-        _write.call(res, JSON.stringify(output));
+        if (body.length) {
+            res.setHeader('content-length', body.length);
+            _write.call(res, body);
+        }
     }
 })
 
-app.use((req, res) => {
-    req.body = CryptoJS.AES.decrypt(req.body.data, "test").toString(CryptoJS.enc.Utf8);
+proxy.on("error", (err, req, res) => {
+    res.end(err.message);
+})
+
+app.post('/key_exchange', (req, res) => {
+    keysMap[req.ip] = server_base.computeSecret(Buffer.from(req.body.key, "hex"));
+    res.json({ key: master_public_key });
+})
+
+app.use(async (req, res) => {
+    console.log(req.body);
+    // if (!keysMap[req.ip])
+    //     await fetch(`${req.ip}:8080/key_exchange`, { method: 'POST', body: { key: server_base.generateKeys() } });
+    req.body = CryptoJS.AES.decrypt(req.body.data, keysMap[req.ip].toString("hex")).toString(CryptoJS.enc.Utf8);
     req.body = JSON.parse(req.body);
     proxy.web(req, res, { target: 'http://127.0.0.1:8181' })
 });
